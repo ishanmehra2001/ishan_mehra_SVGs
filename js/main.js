df9cb1213a9441ba8c9b4da3a44ef9d28ea3746d
ishanmehra2001 <145382231+ishanmehra2001@users.noreply.github.com>
2024-01-30 21:52:45 -0500

    edited and added required files
    edited code and added gitkeep, gitignore files

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 //The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console.
-console.log("JS file connected");
+// console.log("JS file connected");
 
 
-const vectorgraphic = document.querySelector('#badge')
+// const vectorgraphic = document.querySelector('#badge')
 
 
-function logThisId(){
-    console.log(this)
-    console.log('clicked on this element',this.id);
+// function logThisId(){
+//     console.log(this)
+//     console.log('clicked on this element',this.id);
 
-}
+// }
 
-vectorgraphic.addEventListener('click',logTghisId);
+// vectorgraphic.addEventListener('click',logTghisId);
